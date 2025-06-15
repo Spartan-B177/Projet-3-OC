70969494d50f8aae20a0a7f475294ddfed614f9d
Jeremy Laval <yannS117@yahoo.fr>
2025-06-15 18:08:19 +0200

    Modifications login et ajout fenetre modale

--- FrontEnd/index.js
+++ FrontEnd/index.js
@@ -74,4 +74,77 @@ boutonHotelsRestaurants.addEventListener("click", () => {
 
 if (window.localStorage.getItem("token") !== null) {
     console.log("Vous etes connecté !")
+
+    const logout = document.querySelector(".login");
+
+    logout.innerText = "Logout";
+    logout.href = "index.html";
+    logout.addEventListener("click", () => {
+        window.localStorage.removeItem("token")
+    });
+
+    const body = document.getElementById("body")
+    const modeEdition = document.createElement("div")
+    modeEdition.classList.add("edition")
+
+    const modeEditionText = document.createElement("p")
+    modeEditionText.innerText = "Mode Edition"
+
+    const modeEditionIcon = document.createElement("i")
+    modeEditionIcon.classList.add("fa-regular", "fa-pen-to-square")
+
+    modeEdition.appendChild(modeEditionIcon)
+    modeEdition.appendChild(modeEditionText)
+    body.prepend(modeEdition)
+
+    const filtresProjets = document.querySelector(".filtres")
+    filtresProjets.innerHTML = " "
+
+    const mesProjets = document.querySelector(".projets-box");
+
+    const modifierIcon = document.createElement("i")
+    modifierIcon.classList.add("fa-regular", "fa-pen-to-square")
+    const modifierProjets = document.createElement("a");
+    modifierProjets.classList.add("modal-trigger")
+    modifierProjets.innerText = "modifier"
+    modifierProjets.href = "#"
+    
+    mesProjets.appendChild(modifierIcon)
+    mesProjets.appendChild(modifierProjets)
+
+    const modalBox = document.querySelector(".modal-box");
+    const modalTriggers = document.querySelectorAll(".modal-trigger");
+
+    modalTriggers.forEach(trigger => trigger.addEventListener("click", activerModal))
+
+    function activerModal(){
+        modalBox.classList.toggle("active")
+    }
+
+    
+    function genererModalPics(works){
+        for (let i = 0; i < works.length; i++){
+            const modalPics = document.querySelector(".modal-pics");
+
+            const modalImage = document.createElement("img");
+            modalImage.src = works[i].imageUrl;
+
+            const modalImageSuppr = document.createElement("button")
+            modalImageSuppr.classList.add("modal-image-suppr", "fa-regular", "fa-trash-can")
+
+            modalPics.appendChild(modalImage)
+            modalPics.appendChild(modalImageSuppr)
+        }
+    }
+
+    genererModalPics(works)
+
+    const chargeUtile = JSON.stringify(works)
+    console.log(chargeUtile)
+
+    const response = await fetch("http://localhost:5678/api/works/id" + [i] , {
+        method: "DELETE",
+        headers: { "Content-Type": "application/json"} ,
+        body: works,
+    })
 } 
